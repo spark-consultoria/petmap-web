6c2896004e462b755094476001649eb7a6d8c106
Emerson Silva <emerson_jdss@hotmail.com>
2020-05-31 13:00:18 -0300

    config mock partners

---
+++ src/mock/index.js
@@ -0,0 +1,45 @@
+const partners = [
+  {
+    id: Math.random(),
+    name: 'PetShopt da Ana',
+    photos: [
+      'https://studiotec.com.br/blog/wp-content/uploads/2018/05/petshop-capa.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmqBexEMEfGF6V7oVmOWRebSRvzTC-35Lg2ZG6tVzlFCgVzfRv&usqp=CAU'
+    ],
+    address: {
+      street: 'Rua X',
+      neighborhood: 'Santa clara',
+      number: 134
+    }
+  },
+  {
+    id: Math.random(),
+    name: 'PetShopt da Ana',
+    photos: [
+      'https://studiotec.com.br/blog/wp-content/uploads/2018/05/petshop-capa.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmqBexEMEfGF6V7oVmOWRebSRvzTC-35Lg2ZG6tVzlFCgVzfRv&usqp=CAU'
+    ],
+    address: {
+      street: 'Rua X',
+      neighborhood: 'Santa clara',
+      number: 134
+    }
+  },
+  {
+    id: Math.random(),
+    name: 'PetShopt da Ana',
+    photos: [
+      'https://studiotec.com.br/blog/wp-content/uploads/2018/05/petshop-capa.jpg',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmqBexEMEfGF6V7oVmOWRebSRvzTC-35Lg2ZG6tVzlFCgVzfRv&usqp=CAU'
+    ],
+    address: {
+      street: 'Rua X',
+      neighborhood: 'Santa clara',
+      number: 134
+    }
+  }
+];
+
+module.exports = {
+  partners
+};
