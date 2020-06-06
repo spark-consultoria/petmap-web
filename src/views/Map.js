8d1ebf730d5309b2a8898c0fe84d4e2cfd3381b0
Emerson Silva <emerson_jdss@hotmail.com>
2020-06-05 23:53:33 -0300

    config partners table wip

--- src/views/Map.js
+++ src/views/Map.js
@@ -45,100 +45,34 @@ class Map extends React.Component {
                 ))}
 
                 <CardBody>
-                  <Table responsive>
+                  <Table striped>
                     <thead>
                       <tr>
-                        <th className="text-center">#</th>
-                        <th>Name</th>
-                        <th>Level</th>
-                        <th className="text-center">Since</th>
-                        <th className="text-right">Salary</th>
-                        <th className="text-right">Actions</th>
+                        <th>#</th>
+                        <th>Nome do Local</th>
+                        <th>Telefone</th>
+                        <th>Parceiro</th>
+                        <th>Endereço</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td className="text-center">1</td>
-                        <td>Andrew Mike</td>
-                        <td>Develop</td>
-                        <td className="text-center">2013</td>
-                        <td className="text-right">€ 99,225</td>
-                        <td className="text-right">
-                          <Button className="btn-icon" color="info" size="sm">
-                            <i className="fa fa-user"></i>
-                          </Button>
-                          {` `}
-                          <Button
-                            className="btn-icon"
-                            color="success"
-                            size="sm">
-                            <i className="fa fa-edit"></i>
-                          </Button>
-                          {` `}
-                          <Button className="btn-icon" color="danger" size="sm">
-                            <i className="fa fa-times" />
-                          </Button>
-                        </td>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
                       </tr>
                       <tr>
-                        <td className="text-center">2</td>
-                        <td>Manuel Rico</td>
-                        <td>Manager</td>
-                        <td className="text-center">2012</td>
-                        <td className="text-right">€ 99,201</td>
-                        <td className="text-right">
-                          <Button
-                            className="btn-icon btn-round"
-                            color="info"
-                            size="sm">
-                            <i className="fa fa-user"></i>
-                          </Button>
-                          {` `}
-                          <Button
-                            className="btn-icon btn-round"
-                            color="success"
-                            size="sm">
-                            <i className="fa fa-edit"></i>
-                          </Button>
-                          {` `}
-                          <Button
-                            className="btn-icon btn-round"
-                            color="danger"
-                            size="sm">
-                            <i className="fa fa-times" />
-                          </Button>
-                          {` `}
-                        </td>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
                       </tr>
                       <tr>
-                        <td className="text-center">3</td>
-                        <td>Alex Mike</td>
-                        <td>Designer</td>
-                        <td className="text-center">2012</td>
-                        <td className="text-right">€ 99,201</td>
-                        <td className="text-right">
-                          <Button
-                            className="btn-icon btn-simple"
-                            color="info"
-                            size="sm">
-                            <i className="fa fa-user"></i>
-                          </Button>
-                          {` `}
-                          <Button
-                            className="btn-icon btn-simple"
-                            color="success"
-                            size="sm">
-                            <i className="fa fa-edit"></i>
-                          </Button>
-                          {` `}
-                          <Button
-                            className="btn-icon btn-simple"
-                            color="danger"
-                            size="sm">
-                            <i className="fa fa-times" />
-                          </Button>
-                          {` `}
-                        </td>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
                       </tr>
                     </tbody>
                   </Table>
