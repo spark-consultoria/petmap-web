4d315593d4cbedd38ae88235e50aa4e1c4f277b5
Emerson Silva <emerson_jdss@hotmail.com>
2020-08-27 19:42:52 -0300

    change mock file

--- src/mock/index.js
+++ src/mock/index.js
@@ -6,7 +6,7 @@ const partners = [
       'https://studiotec.com.br/blog/wp-content/uploads/2018/05/petshop-capa.jpg',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmqBexEMEfGF6V7oVmOWRebSRvzTC-35Lg2ZG6tVzlFCgVzfRv&usqp=CAU'
     ],
-    phone: '4223-2222',
+    phone: '4223-2229',
     partner: true,
     address: {
       street: 'Rua X',
