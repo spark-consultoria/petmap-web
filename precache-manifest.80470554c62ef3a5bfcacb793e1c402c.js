244acef9d411d81b876ff2266508ea9271c02116
Emerson Silva <emerson_jdss@hotmail.com>
2020-09-02 16:11:26 -0300

    Updates

---
+++ precache-manifest.80470554c62ef3a5bfcacb793e1c402c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802823f7d332ca8d7370412393ae3204",
+    "url": "/petmap-web/index.html"
+  },
+  {
+    "revision": "04ed76b96d7cc532bc9f",
+    "url": "/petmap-web/static/css/2.50f53c61.chunk.css"
+  },
+  {
+    "revision": "08098bc65bb1a65fe380",
+    "url": "/petmap-web/static/css/main.adf97d5f.chunk.css"
+  },
+  {
+    "revision": "04ed76b96d7cc532bc9f",
+    "url": "/petmap-web/static/js/2.e835edea.chunk.js"
+  },
+  {
+    "revision": "a3082971f25732d4c0569f288ba70658",
+    "url": "/petmap-web/static/js/2.e835edea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08098bc65bb1a65fe380",
+    "url": "/petmap-web/static/js/main.af56ef03.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/petmap-web/static/js/main.af56ef03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed135c2616163b1d26b1",
+    "url": "/petmap-web/static/js/runtime-main.c38f8f59.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/petmap-web/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/petmap-web/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/petmap-web/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/petmap-web/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/petmap-web/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/petmap-web/static/media/react-logo.eb6be414.png"
+  }
+]);
